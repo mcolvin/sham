5eaef3c30adb1cd4888d1712dbd5b6ad2dcf99c1
mcolvin <colvin.mike@gmail.com>
2017-04-27 08:48:33 -0500

    other stuff
    asdfadsf

--- test.R
+++ test.R
@@ -7,3 +7,13 @@ fit<- lm(y~x)
 par(mfrow=c(2,2))
 plot(fit,which = 1:4,caption = c("Residuals",'QQ','Scale','Cooks D'))
 
+add stuff and whatnot
+
+asdfadf
+
+asdfasd
+
+
+asdkjf;aksldjf;ajkdf
+
+asdfasdf
